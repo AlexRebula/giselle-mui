9163b253464d069e766ed1d7c25a51147bc1d70c
ZigaRebula <ziga@littlebranches.io>
2026-07-28 11:39:29 +1000

    feat(profile-summary-card): add imports

---
+++ src/components/material/surfaces/card/profile-summary/profile-summary-card.tsx
@@ -0,0 +1,9 @@
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
+import type { ProfileSummaryCardProps } from './types';
+
+// ----------------------------------------------------------------------
